d2158c3ae8a6e8ae0c476cab74f8052b7dc2848b
Dobrynia100 <dobrynya2001@gmail.com>
2023-05-27 19:08:00 +0300

    Dropper_animation

--- Assets/scripts/TrigCubeDisp.cs
+++ Assets/scripts/TrigCubeDisp.cs
@@ -12,15 +12,27 @@ public class TrigCubeDisp : MonoBehaviour
     public Transform spawnPoint;
     [SerializeField]
     private AudioSource audioC;
-    private bool cubeSpawned = false;
+    [SerializeField]
+    private GameObject cubedropper;
+    public bool cubeSpawned = false;
+    private bool IsOpen = false;
+    private void Start()
+    {
+        IsOpen = false;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && !cubeSpawned)
         {
             cubeSpawned = true;
+            IsOpen= true;
+            cubedropper.GetComponent<Animator>().SetBool("open", IsOpen);
             GameObject newCube = Instantiate(cubePrefab, spawnPoint.position, spawnPoint.rotation);
             audioC.Play();
+            
+
         }
+       
     }
 
 }
